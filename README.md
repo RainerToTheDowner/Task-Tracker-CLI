b44f8481ea03d690c50b22da2f0333ab1dadc10f
RainerToTheDowner <ryudragon2011@gmail.com>
2026-01-08 07:25:24 -0800

    Create and Load  JSON Data

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Task-Tracker-CLI
 CLI Application that tracks tasks.
+
+Project Page: https://roadmap.sh/projects/task-tracker
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+import json
+import os
+
+tasks = {}
+
+if os.path.exists("tasks.json") == False:
+    with open("tasks.json", "w") as jsonfile:
+        json.dump(tasks, jsonfile, indent=4)
+else:
+    with open("tasks.json", "r") as jsonfile:
+        tasks = json.load(jsonfile)
