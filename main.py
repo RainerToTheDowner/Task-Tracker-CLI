5876d13353d3242501888d41c9fa4cd50be4181f
RainerToTheDowner <ryudragon2011@gmail.com>
2026-01-13 07:23:40 -0800

    Update Tasks

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ userinput = ""
 
 while userinput != "exit":
     userinput = input()
+    wordsInUserInput = userinput.split()
+    command = wordsInUserInput[0].lower()
 
-    if userinput[:3].lower() == "add":
+    if command == "add":
         if (len(list(tasks.keys())) > 0):
-            taskID = list(tasks.keys())[-1]+1
+            taskID = int(list(tasks.keys())[-1])+1
         else:
             taskID = 1
 
@@ -31,3 +33,16 @@ while userinput != "exit":
             json.dump(tasks, jsonfile, indent=4)
 
         print("Task added successfully (ID: " + str(taskID) + ")")
+    elif command == "update":
+        possibleID = None
+        possibleNewName = None
+        if len(wordsInUserInput) > 2:
+            possibleID = wordsInUserInput[1]
+            possibleNewName = (" ".join(wordsInUserInput[2:])).replace('"', "")
+        try:
+            tasks[possibleID]["description"] = possibleNewName
+            with open("tasks.json", "w") as jsonfile:
+                json.dump(tasks, jsonfile, indent=4)
+            print("Updated ID " + possibleID + ' to "' + possibleNewName + '"')
+        except:
+            print("ID does not exist or no name was given")
